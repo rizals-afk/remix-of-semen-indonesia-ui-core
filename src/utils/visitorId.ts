f2d4d9475124b0453f0b3b49d0b3499e9d6d6335
Rizal M. S <rizals@nurosoft.id>
2026-08-19 09:10:53 +0700

    update

--- src/utils/visitorId.ts
+++ src/utils/visitorId.ts
@@ -1,7 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export function getVisitorId(): string {
   let id = localStorage.getItem('visitor_id');
   if (!id) {
-    id = crypto.randomUUID();
+    id = uuidv4();
     localStorage.setItem('visitor_id', id);
   }
   return id;
